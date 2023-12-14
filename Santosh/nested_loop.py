d13776599b916214ca1ec7bbfc35a10a5ea340ac
santoshwadikar <santoshmw.11@gmail.com>
2023-12-14 09:51:13 +0530

    adiing if loop

---
+++ Santosh/nested_loop.py
@@ -0,0 +1,12 @@
+for i in range(1,11):
+    print("address i:", i)
+    for j in range(1,4):
+        #print("address :",i)
+        print("package :" ,j)
+    print("*"*40)
+
+###############################################################
+for num in range(1,100):
+    prime= True
+
+    
