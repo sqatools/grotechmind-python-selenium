1c58861e3133dc8b842cd77b7a74081c98683c9a
tusharaher3131 <tusharaher3131@gmail.com>
2023-12-18 07:51:12 +0530

    added

---
+++ Tushar/Practice_12DEC.py
@@ -0,0 +1,14 @@
+### write programme to cal factorial of any given no##############
+
+#5=5*4*3*2*1
+num=6
+fact=1
+for i in range (num,0,-1):
+    print(i)
+    fact=fact*i
+    print("fact:" ,fact)
+
+###writ a programme to print fabonaci series
+#0,1,1,2,3,5,8,13,21
+
+
