cc3ec73304a371d651b60767f0dde260b35d0b3a
unknown <rvankita@gmail.com>
2023-12-20 08:08:18 +0530

    String file

--- Ankita Verma/AV1/IfelsePractice.py
+++ Ankita Verma/AV1/IfelsePractice.py
@@ -1,5 +1,6 @@
 #Ifelse condition
-#Number is divided by 3 or not
+#******************#Program 1*****************************
+# Number is divided by 3 or not
 
 '''num = input("Please enter number:")
 if num%3 == 0:
@@ -10,13 +11,14 @@ else:
 
 
 
-"""Print number divide bye 3"""
+#***********************Program 2"""Print number divide bye 3"""*************************
 
 
 """for i in range(1,51):
     if i%3==0:
         print(i, "Number is divisible by 3")"""
 
+ #*********************Program 3************************************
  ##If else program to get all the numbers divided by 5 from 1 to 30.
 
 """for i in range(1,31):
@@ -24,6 +26,7 @@ else:
         print("Number is divisible",i)"""
 
 
+#********************************PROGRAM 4 ***************************************
 #f else program to assign grades as per total marks.
 """marks > 40: Fail
 marks 40 – 50: grade C
@@ -35,7 +38,7 @@ marks 90 – 100: grade Excellent
 marks > 100: Invalid marks"""
 
 
-user_Input = int(input("Enter Grades "))
+"""user_Input = int(input("Enter Grades "))
 if user_Input<40:
     print("Fail")
 elif user_Input>=40 and user_Input<=50:
@@ -51,4 +54,96 @@ elif user_Input>=80 and user_Input<=90:
 elif user_Input<= 100:
      print(" Excellent")
 elif user_Input>100:
-     print(" Invalid marks")
+     print(" Invalid marks")"""
+
+#***************************************PROGRAM 5********************************************
+#Check Number is in Fibonacci list or not
+
+"""Fib_Series = [0,1,1,2,3,4,5,6,7,8,9,10,11]
+
+check_Fibo = int(input("Enter Number "))
+
+if check_Fibo in Fib_Series:
+    print("Number is in Fibonacci")
+else:
+    print("Not in given list")"""
+
+#doubt when entering 0 why not printing fib
+
+
+#**********************************PROGRAM 6 *******************
+#Add even and odd number
+"""even_sum=0
+odd_sum=0
+for i in range (1,51):
+    if i%2==0:
+        even_sum=even_sum+i
+        print("number is even",i)
+    else:
+
+        print ("number is odd",i)"""
+
+#**********************************PROGRAM 7*****************************
+#Accept the Age and display the eligible or not
+"""for i in range(1,11):
+     Age=int(input("Enter your Age  "))
+      if Age<=10:
+        print("You are Eligible")
+      else:
+        print("you are not Eligible ")"""
+
+
+# *********************Program 8 Accept Age and Name from 4 user and Display youngest one
+#doubt ????
+
+"""for i in range(0,2):
+     age=int(input("Enter your  Age"))
+     name=str(input("Enter your Name "))
+     print("Name and Age",name,age)
+
+if age<=15:
+     print("Youngest", age)
+else:
+     print("Older than 15")"""
+
+#******************Program 9 ************************************
+#Print a square of Input Number
+
+"""num2=int(input("Enter Number"))
+if num2%11==0:
+    print("number is square",num2**2)
+else:
+    print("false")"""
+
+#*************************************Program10*******************
+#Program to check the authentication of username and password
+
+"""username=str(input("Enter username:- "))
+pwd=str(input("Enter Password:-  "))
+if username == pwd:
+    print("Authentication done!")
+else:
+    print("Authentication Failed!")"""
+
+#***********************************Program 11***************************************
+#Python program to validate user_id in the list of user_ids.
+
+#userid = [001,002,003,004,005,006](why cant we take 001 Doubt
+"""userid = [1,2,3,4,5,6]
+user_id = int(input("Enter userid :- "))
+if user_id in userid:
+    print("User is in the List")
+else:
+    print("not in the list")"""
+
+#************************Program 12*************************************
+
+
+
+
+
+
+
+
+
+
---
+++ Ankita Verma/AV1/String_practice.py
@@ -0,0 +1,27 @@
+#String Introduction
+
+#String is ordered and immmutable
+f_string= "Hello World"
+len( "Hello World")
+print(len("Hello World"))
+
+s_string="I AM LEARNING pYTHON"
+add_string=str("Hello World" ""+" I AM LEARNING pYTHON ")
+print(add_string)
+print(len(add_string))
+
+print(f_string[0])
+print(f_string[1])
+print(f_string[2])
+print(f_string[3])
+print(f_string[4])
+
+print(f_string[0])
+print(f_string[-1])
+print(f_string[-2])
+print(f_string[-3])
+print(f_string[-4])
+
+#Slicing: Access a range of characters in a string by using the slicing operator colon :
+
+print(f_string[0:8:3])
