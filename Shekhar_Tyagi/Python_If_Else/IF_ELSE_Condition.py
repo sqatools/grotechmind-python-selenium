0213dc1ec5add494415f988a832af9cba9d66706
Shekhartyagi112 <shekhartyagi53@gmail.com>
2023-12-25 13:24:58 +0530

    update

--- Shekhar_Tyagi/Python_If_Else/IF_ELSE_Condition.py
+++ Shekhar_Tyagi/Python_If_Else/IF_ELSE_Condition.py
@@ -30,7 +30,7 @@ marks > 100: Invalid marks
 """
 
 print("3). If else program to assign grades as per total marks.")
-marks = int(input("enter the value : "))
+marks = 79 #int(input("enter the value : "))
 if marks<40:
 	print("marks: " , marks , ", Failed")
 
@@ -49,6 +49,64 @@ elif marks >=90 and marks <=100:
 	print("marks:",marks, "Grade Excellent")
 else:
 	print("marks: ", marks, ": Invalid marks")
+
+# 4). Python program to check the given number divided by 3 and 5.
+print("4). Python program to check the given number divided by 3 and 5.")
+
+num1 = 15 #int(input("enter the num: "))
+if num1%3==0 and num1%5 == 0:
+	print(f"{num1} is divisable  by 3 and 5: ",num1)
+else:
+	print(f"{num1} is not divisable by 3 and 5")
+
+
+# 5). Python program to print the square of the number if it is divided by 11.
+print("5). Python program to print the square of the number if it is divided by 11.")
+
+num =  22 #int(input("Enter a number: "))
+if num%11 == 0:
+	print(num**2)
+else:
+	print(f"{num} is not divisable by 11")
+
+# 6). Python program to check given number is a prime number or not.
+print("6). Python program to check given number is a prime number or not.")
+
+num = 11 # int(input("enter the value: "))
+count = 0
+for i in range (2 , num):
+	if num%i == 0:
+		count=count+1
+if count > 0:
+	print(f"{num} is not a prime number")
+else:
+	print(f"{num} is a prime number")
+
+# 7). Python program to check given number is odd or even.
+print("7). Python program to check given number is odd or even.")
+
+num = int(input("enter the number:"))
+if num%2 == 0:
+	print(f"{num} is a even number")
+else:
+	print("num: " ,f"{num} is odd number")
+
+# 8). Python program to check a given number is part of the Fibonacci series from 1 to 10.
+print("8). Python program to check a given number is part of the Fibonacci series from 1 to 10.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
