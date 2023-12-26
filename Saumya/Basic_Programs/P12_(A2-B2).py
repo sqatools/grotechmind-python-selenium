00dce10a02455ec0c5827171f1592f99e9a992f1
AutomationTesterSaumya28 <saumyam050@gmail.com>
2023-12-26 19:46:59 +0530

    add basic files to git 26 dec

---
+++ Saumya/Basic_Programs/P12_(A2-B2).py
@@ -0,0 +1,8 @@
+"""
+12). Python program to solve the given math formula.
+Formula : a2 – b2 = (a-b)(a+b)
+"""
+a = 3
+b = 2
+result = (a-b)*(a+b)
+print("a2-b2 :", result)
---
+++ Saumya/Basic_Programs/P9_Pythagorous.py
@@ -0,0 +1,16 @@
+"""
+9). Python program to solve this Pythagorous theorem.
+Theorem : (a2 + b2 = c2)
+
+ import math
+ a = input("enter side1 :")
+ b = input("enter side2 :")
+ hypo = a**2+b**2
+"""
+
+import math
+side1 = 5
+side2 = 4
+
+hypo = a**2+b**2
+print("Hypotenious side: ",math.sqrt(hypo)) 
