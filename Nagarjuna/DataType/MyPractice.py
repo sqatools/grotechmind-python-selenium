e14399f8f611216b60e0751392144f6ce594f8a5
nagarjunaM <nagarjunarpl@gmail.com>
2024-01-09 15:39:34 +0530

    updating

--- Nagarjuna/DataType/MyPractice.py
+++ Nagarjuna/DataType/MyPractice.py
@@ -7,6 +7,27 @@ print(b)
 c='''this is a another string '''
 print(c)
 
+# python program to update a character of a string
+string = "hello world"
+print("initial string :")
+print(string)
+#updating the character in hello
+list1 = list(string)
+list1[2]= 'n'
+string1 = ''.join(list1)
+print(string1)
+
+## update entire string
+string= " hello i am ok"
+print("initial string: ")
+print(string)
+#updating
+string =" i have done"
+print("\n updated string: ")
+print(string)
+
+
+""" 
 # list
 list = [1,"a","string",1+2]
 print(list)
@@ -36,3 +57,23 @@ for i in range(0,10):
 # reverse the string
 abc = "mynameis"
 print(abc[::-1])
+
+"""
+# tuple
+tuple1=()
+print("initial empty tuple")
+print(tuple1)
+tuple1= ('naga','abc')
+print("tuple with string" )
+print(tuple1)
+list=[1,2,3,4,6]
+print("tuple with list: ")
+print(tuple(list))
+tuple1= tuple('random')
+print(tuple1)
+
+tuple2=(1,2,3,4,5)
+tuple3=('python','learning')
+tuple4= (tuple2+tuple3)
+print(tuple4)
+
