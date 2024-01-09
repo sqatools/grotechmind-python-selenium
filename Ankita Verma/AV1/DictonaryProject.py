5cbf907b5337b08b3a1a1f373fc424567ab42ca8
Ankita <rvankita@gmail.com>
2024-01-09 12:15:45 +0530

    file updated

--- Ankita Verma/AV1/DictonaryProject.py
+++ Ankita Verma/AV1/DictonaryProject.py
@@ -1,16 +1,16 @@
 #Enter Class and Student Names
 
-input("Select your Class 1st to 5th " )
+
+#input("Select your Class 1st to 5th " )
+var1 = input("Select your Class 1st to 5th " )
 classes=['1','2','3','4','5']
 for i in range(len(classes)):
-    if classes==1:
-        input("Enter Student Name ",student1)
-        student1={ "Name: "", classes:""}
+    if classes[i]==var1:
+        #Name = input("Enter Student Name ")
+        #classs = input("Enter Class Name ")
+        student1={ "Name":'Rahul', "classe":'john'}
         print(student1)
-    if classes==2:
-        student2={ "Name: "", classes:""}
-        print(student2)
-    if classes==3:
-        student3={ "Name: "", classes:""}
-        print(student3)
+    else:
+        print("user input didn't match")
+
 
