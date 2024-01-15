a5110c343163deb9df0decd45d3097efc9adc940
Ankita <rvankita@gmail.com>
2024-01-15 09:14:59 +0530

    new file

---
+++ Ankita Verma/AV1/Function_program.py
@@ -0,0 +1,28 @@
+# Function helps to organize code into logical block
+# without Parameter
+# Exercise 1: Create a function in Python
+"""def greet():
+    print("Hello")
+greet()"""
+
+#Function with Argument
+def call_name(name):
+    print("hello",name)
+call_name("Ankita")
+
+# Write a Python function Calsi
+def add(x,y):
+return x+y
+
+def sub(x,y):
+return  x-y
+
+def Mul(x,y);
+return x*y
+
+def div(x,y)
+
+
+
+
+
