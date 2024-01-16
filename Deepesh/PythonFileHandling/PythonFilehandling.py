a44d47c4023e0d0e17276d58d1237b7496f5f6c9
Bi12Python <xovek11875@bitvoo.com>
2024-01-16 09:27:11 +0530

    File handling changes practice

--- Deepesh/PythonFileHandling/PythonFilehandling.py
+++ Deepesh/PythonFileHandling/PythonFilehandling.py
@@ -62,3 +62,128 @@ def append_content_file(filename, content):
 
 append_content_file("test_file.txt", "Grotech Mind Learning")
 append_content_file("test_file5.txt", "Growtech Mind Learning")
+
+
+#  context manager
+def read_file_data(filename):
+    with open(filename) as file1:
+        data = file1.read()
+        print(data)
+        print("close status :", file1.closed)
+        print("filename :", file1.name)
+        print("filemode :", file1.mode)
+
+    print("close status :", file1.closed)
+
+
+#read_file_data("read_file.txt")
+
+# read file content
+# -> read specific number of characters/bytes
+# -> Read specific number of lines
+# -> Read all the lines in the list
+
+# # -> read specific number of characters/bytes
+def read_specific_char(filename, char_count):
+     with open(filename, 'r') as file:
+         data = file.read(char_count)
+         print(data)
+
+# read_specific_char("read_file.txt", 20)
+# Python offers many c
+
+# -> Read specific number of lines with readline method.
+def read_specific_lines(filename,line_num):
+    with open(filename, "r") as file:
+        for i in range(line_num):
+            data1 = file.readline()
+            print(data1, end="")
+
+#read_specific_lines("read_file.txt", 5)
+
+
+# read list of lines with  help of readlines
+
+def read_lines_list(filename, line_number):
+    with open(filename, "r") as file:
+        if line_number > 0:
+            line_number = line_number -1
+        lines_list = file.readlines()
+        #print(lines_list)
+        print(lines_list[line_number])
+
+
+#read_lines_list("read_file.txt", 5)
+#read_lines_list("read_file.txt", -1)
+
+
+def read_lines_list_specific_range(filename, start, end):
+    with open(filename, "r") as file:
+        lines_list = file.readlines()
+        #print(lines_list[start-1:end])
+        for i in range(start-1, end):
+            print(lines_list[i], end="")
+
+
+#read_lines_list_specific_range("read_file.txt", 2, 5)
+
+
+# tell method and seek method
+
+# tell method : this method provides current position of the curser
+# seek method : this method helps to move cursor position
+
+def read_file_with_tell(filename):
+    with open(filename, 'r') as file:
+        print("initial cursor position:", file.tell())
+        data = file.read(10)
+        print("10 byte data :", data)
+        print("cursor position:", file.tell())
+        data20 = file.read(20)
+        print("20 byte data :", data20)
+        print("cursor position:", file.tell())
+
+# read_file_with_tell('read_file.txt')
+
+def read_file_with_seek(filename):
+    with open(filename, "rb") as file:
+        print("initial cursor position:", file.tell())
+        file.seek(20, 0) # start from begining of the file
+        print("cursor position:", file.tell())
+        file.seek(20, 1) # start from current position of the cursor
+        print("cursor position:", file.tell())
+        data = file.read(10)
+        print("data :", data)
+        print("cursor position:", file.tell())
+        file.seek(-50, 2) # set the cursor from end of the file.
+        data = file.read()
+        print("last content of file:", data)
+        print("cursor position:", file.tell())
+
+
+read_file_with_seek("read_file.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
