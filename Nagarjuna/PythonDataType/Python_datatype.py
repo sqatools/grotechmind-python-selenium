d5e72471a9f9de49b9226a0dcee888000a300e88
nagarjunaM <nagarjunarpl@gmail.com>
2024-01-17 09:17:00 +0530

    filehandling

--- Nagarjuna/PythonDataType/Python_datatype.py
+++ Nagarjuna/PythonDataType/Python_datatype.py
@@ -29,3 +29,34 @@ number= 392933
 var = str(number)
 print(var, type(var), var[4])
 ### 392933 <class 'str'> 3
+
+# int -> list # conversion is not possible
+"""
+num = 67546756
+var = list(num)
+print(var)
+"""
+# int -> tuple # conversion is not possible
+# int -> dict  # conversion is not possible
+# int -> set   # # conversion is not possible
+"""
+number = 300
+var = set(number)
+print(var, type(var))
+"""
+
+# Int -> Boolean----
+print("Int -> Boolean")
+num = 765
+bool1 = bool(num)
+print(bool1 , type(bool1))
+# True <class 'bool'>
+
+# if we convert null value in boolean.
+num1 = 0
+bool1 = bool(num1)
+print(bool1, type(bool1))
+# False <class 'bool'>
+
+################# Float #####################
+
---
+++ Nagarjuna/PythonFile Handling/Read_Excel_data.py
@@ -0,0 +1,19 @@
+import openpyxl
+"""
+pip install openpyxl
+"""
+
+def read_excel_file(filepath, row, col):
+    wb_object = openpyxl.load_workbook(filepath)
+    sheet_obj = wb_object.active
+    cell_object = sheet_obj.cell(row=row, column=col)
+    return cell_object.value
+
+file_path = r"D:\Python\GitRepo\grotechmind-python-selenium\Nagarjuna\PythonFile Handling\Test_Data.xlsx"
+val = read_excel_file(file_path, 1, 1)
+print(val)
+
+print("@"*20)
+for i in range(1, 4):
+    val = read_excel_file(file_path, i, 1)
+    print(val)
