419e9264dea7b9317bfeacb2e4152c9544146b93
tusharaher3131 <tusharaher3131@gmail.com>
2024-01-21 07:31:36 +0530

    up

---
+++ Tushar/Read_JSON_Data.py
@@ -0,0 +1,136 @@
+import json
+"""
+def read_emp_json_data(file_path):
+    with open(file_path,'r') as file:
+        data=json.load(file)
+        return data
+result=read_emp_json_data("emp.json")
+print(result)
+
+print(result['Salary'])
+"""
+"""
+def read_json_file(file_path):
+    with open(file_path,'r') as data_file:
+        data=json.load(data_file)
+        return data
+result=read_json_file('emp.json')
+print(result)
+print(result['Salary'])
+"""
+
+"""
+
+def read_student_json_data(file_path):
+    with open(file_path,'r') as data_file:
+        data=json.load(data_file)
+        return data
+result=read_student_json_data('student_data.json')
+print(result)
+print(result['student_marks'])
+"""
+"""
+def read_student_json_data(file_path):
+    with open(file_path,'r') as data_file:
+        data=json.load(data_file)
+        return data
+file_path="student_data.json"
+result=read_student_json_data(file_path)
+print(result)
+print(result['student_name'])
+"""
+"""
+def read_json_data(file_path):
+    with open(file_path,'r') as data_file:
+        data=json.load(data_file)
+        return data
+file_path='student_data.json'
+result=read_json_data(file_path)
+print(result)
+print(result['student_mail'])
+"""
+"""
+def read_student_json_data(file_path):
+    with open(file_path,'r') as data_file:
+        data=json.load(data_file)
+        return data
+file_path='student_data.json'
+result=read_student_json_data(file_path)
+print(result)
+print(result['student_class'])
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
