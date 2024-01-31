855e6c985ddda9895649bbb55ac43cf599d84d3a
tusharaher3131 <tusharaher3131@gmail.com>
2024-01-31 22:27:43 +0530

    up

--- Tushar/Selenium/XPATH_Loacators_Practice.py
+++ Tushar/Selenium/XPATH_Loacators_Practice.py
@@ -30,9 +30,22 @@ driver.implicitly_wait(10)
 
 driver.get("https://automationbysqatools.blogspot.com/2021/05/dummy-website.html")
 
-driver.find_element(By.XPATH, "(//input[@name='firstname'])[1]").send_keys("Rahul")
-driver.find_element(By.XPATH, "(//input[@name='firstname'])[2]").send_keys("Gupta")
-
+driver.find_element(By.XPATH, "(//input[@name='firstname'])[1]").send_keys("Tushar")
+driver.find_element(By.XPATH, "(//input[@name='firstname'])[2]").send_keys("Aher")
+driver.find_element(By.XPATH,"//input[@name='birthday']").send_keys('10-08-1989')
+driver.find_element(By.XPATH,"//input[@id='male']").click()
+driver.find_element(By.XPATH,"//input[@id='female']").click()
+driver.find_element(By.XPATH,"//*[@id='admorepass']").click()
+driver.find_element(By.XPATH,"//*[@name='fromcity']").send_keys("nashik")
+driver.find_element(By.XPATH,"//*[@name='destcity']").send_keys('pune')
+driver.find_element(By.XPATH,"//*[@id='departdate']").send_keys('31-01-2024')
+driver.find_element(By.XPATH,"//*[@id='returndate']").send_keys("15-02-2024")
+driver.find_element(By.XPATH,"//*[@id='visadate']").send_keys('25-02-2024')
 time.sleep(5)
 
 driver.close()
+
+# //*[@attribute='value']
+#//*[@fdprocessedid='avj1c4']
+
+
