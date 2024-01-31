9d333c0a43033abdbf6307a8f4a534883ea6ef54
nagarjunaM <nagarjunarpl@gmail.com>
2024-01-31 08:06:43 +0530

    Selenium

---
+++ Nagarjuna/Selenium_Code/FIrst_program.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get("https://www.google.com/")
+driver.find_element(By.NAME,"q").send_keys("python programming")
+driver.find_element(By.NAME,"btnK").click()
+time.sleep(5)
+
+driver.close()
+
+driver= webdriver.edge()
+driver.maximize_window()
+driver.implicitly_wait(10)
