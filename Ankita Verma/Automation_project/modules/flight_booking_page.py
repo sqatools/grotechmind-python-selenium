5b232555cde74a568d2aa7806948bd0949ece8b3
tusharaher3131 <tusharaher3131@gmail.com>
2024-02-28 16:17:01 +0530

    up

--- Ankita Verma/Automation_project/modules/flight_booking_page.py
+++ Ankita Verma/Automation_project/modules/flight_booking_page.py
@@ -1,9 +1,12 @@
 from selenium_base.selenium_code import SeleniumCode #parentclass
-from flight_booking_page_data import *
+from .flight_booking_page_data import *
 
 class FlightBookingPage(SeleniumCode):
     def __init__(self,driver):
        super().__init__(driver)
+
+    def close_mobile_popup(self):
+        self.click_element(mobile_login_popup)
 
     def enter_source_city(self, city_name):
         self.click_element(source_city_field)
--- Ankita Verma/Automation_project/tests/test_flight_booking.py
+++ Ankita Verma/Automation_project/tests/test_flight_booking.py
@@ -8,5 +8,6 @@ class Testflightbooking:
 
    def test_search_flight(self):
     self.fp=FlightBookingPage(self.driver)
+    self.fp.close_mobile_popup()
     self.fp.enter_source_city("Mumbai")
     time.sleep(10)
