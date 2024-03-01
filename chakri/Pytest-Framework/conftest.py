a1a86d9306e2128b86efbc7f04e1afbf58cf27ff
DurgaBSunkara <daranisunkara96@gmail.com>
2024-02-29 22:12:43 -0500

    update

--- chakri/Pytest-Framework/conftest.py
+++ chakri/Pytest-Framework/conftest.py
@@ -8,3 +8,13 @@ def website():
     driver.maximize_window()
     driver.implicitly_wait(10)
     return driver
+
+
+@pytest.fixture(scope="class")
+def launch_browser(request):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    request.cls.driver = driver
+    yield
+    driver.close()
--- chakri/Pytest-Framework/website/goibibo.py
+++ chakri/Pytest-Framework/website/goibibo.py
@@ -3,19 +3,20 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+
 @pytest.mark.usefixtures("launch_browser")
-class TestGoibibo:
+class TestHotelBooking:
 
-def test_search_flight(self):
+    def test_search_flight(self):
 
-    self.driver.get("https://www.goibibo.com/")
-    element = self.driver.find_element(By.XPATH,"//span[@class='logSprite icClose']")
-    if element.is_displayed():
-        element.click()
+        self.driver.get("https://www.goibibo.com/")
+        element = self.driver.find_element(By.XPATH, "//span[@class='logSprite icClose']")
+        if element.is_displayed():
+            element.click()
 
-    self.driver.find_element(By.XPATH,"//span[text()='From']//following-sibling::p").click()
-    self.driver.find_element(By.XPATH,"//span[text()='From']//following-sibling::p").sendkeys("Mumbai,India")
-    time.sleep(10)
+        self.driver.find_element(By.XPATH, "//span[text()='From']//following-sibling::p").click()
+        self.driver.find_element(By.XPATH, "//span[text()='From']//following-sibling::input").send_keys("Mumbai")
+        time.sleep(5)
 
 
 
