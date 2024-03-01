b1e858f98f3079822b83e8e9855e66b01aed0499
ChakriSiragam <chakrisiragam66@gmail.com>
2024-03-01 08:02:05 +0530

    pytest class

---
+++ chakri/Pytest-Framework/website/goibibo.py
@@ -0,0 +1,21 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+
+@pytest.mark.usefixtures("launch_browser")
+class TestGoibibo:
+
+def test_search_flight(self):
+
+    self.driver.get("https://www.goibibo.com/")
+    element = self.driver.find_element(By.XPATH,"//span[@class='logSprite icClose']")
+    if element.is_displayed():
+        element.click()
+
+    self.driver.find_element(By.XPATH,"//span[text()='From']//following-sibling::p").click()
+    self.driver.find_element(By.XPATH,"//span[text()='From']//following-sibling::p").sendkeys("Mumbai,India")
+    time.sleep(10)
+
+
+
