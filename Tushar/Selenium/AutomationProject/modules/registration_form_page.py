4f459f854d94c7d066aec89caf4bcc0aef01b27f
tusharaher3131 <tusharaher3131@gmail.com>
2024-03-06 08:02:55 +0530

    up

---
+++ Tushar/Selenium/AutomationProject/modules/registration_form_page.py
@@ -0,0 +1,19 @@
+from selenium_base.selenium_code import seleniumcode
+from .registration_form_page_data import *
+
+class Regformpage(seleniumcode):
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    def enter_email_password(self,email_id,password):
+        self.fill_data(email,email_id) # def fill_data(self,locator,data):
+        self.fill_data(pwd,password)
+
+    def select_male_female_radio_button(self,person='female'):
+        if person=='male':
+            self.click_element(male_radio)
+        else:
+            self.click_element(female_radio)
+
+    def select_skill_dropdown(self,value):
+        self.select_value_from_dropdown(select_skil_dd,value)
---
+++ Tushar/Selenium/AutomationProject/modules/registration_form_page_data.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+email=(By.XPATH,"//input[@id='email']")
+pwd=(By.XPATH,"//input[@id='password']")
+male_radio=(By.XPATH,"//input[@id='male']")
+female_radio=(By.XPATH,"//input[@id='Female']")
+select_skil_dd=(By.XPATH,"(//div[@class='select form-control'])[1]")
+select_selenium_dd=(By.XPATH,"//div[@class='skills caret caret-rotate']")
+select_country_dd=(By,"//div[@class='Country caret']")
+present_address=(By.XPATH,"//textarea[@class='Present-Address form-control']")
+parement_addres=(By.XPATH,"//textarea[@class='Permanent-Address form-control']")
+pincode=(By.XPATH,"//input[@id='Pincode']")
+religion=(By.XPATH,"//ul[@id='Religion']")
+choose_file=(By.XPATH,"//input[@id='file']")
